19d1c156fe2475f533daba7ad58597a85a5c3928
Bardia Amjadi <bardia.amjadi88@gmail.com>
2025-07-24 11:58:23 +0330

    Delete root directory

--- root/src/js/zoom.js
+++
@@ -1,7 +0,0 @@
-const imageModal = document.getElementById('imageModal');
-  imageModal.addEventListener('show.bs.modal', function (event) {
-    const trigger = event.relatedTarget;
-    const imgSrc = trigger.getAttribute('data-img-src');
-    const modalImg = imageModal.querySelector('#modalImage');
-    modalImg.src = imgSrc;
-  });
